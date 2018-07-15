cd1c3ef7471a29bc75f5f483785980e46653e9ee
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-15 23:23:50 +0700

    Edit SpringXMLConfigDemo

--- src/main/java/com/higgsup/intern/spring/demo/xmlconf/SpringXMLConfigDemo.java
+++ src/main/java/com/higgsup/intern/spring/demo/xmlconf/SpringXMLConfigDemo.java
@@ -1,37 +1,42 @@
 package com.higgsup.intern.spring.demo.xmlconf;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.higgsup.intern.spring.demo.pojo.Dictionary;
 
 import java.util.Scanner;
 
 public class SpringXMLConfigDemo {
     public static void main(String[] args) {
-        ApplicationContext appContext = new ClassPathXmlApplicationContext("spring-beans.xml");
+       /* ApplicationContext appContext = new ClassPathXmlApplicationContext("spring-beans.xml");
+        Dictionary dict = appContext.getBean("dictionary", Dictionary.class);*/
 
-
-
-
-
+        Dictionary dict = new Dictionary();
         Scanner sc = new Scanner(System.in);
-        String input = new String();
+        String line;
         String[] lines;
 
+        dict.getDictionaryFromFile();
+        dict.instruct();
         do {
-            System.out.println("> ");
-            String line = sc.nextLine();
+            System.out.print("> ");
+            line = sc.nextLine();
             lines = line.split(" ", 3);
+
             switch (lines[0]) {
                 case "save": {
+                    dict.save();
                     break;
                 }
                 case "add": {
+                    String lines1 = lines[1].substring(0, lines[1].length() - 1);
+                    dict.add(lines1, lines[2]);
                     break;
                 }
                 case "lookup": {
+                    dict.lookup(lines[1]);
                     break;
                 }
                 case "delete": {
+                    dict.delete(lines[1]);
                     break;
                 }
                 case "quit": {
@@ -42,6 +47,6 @@ public class SpringXMLConfigDemo {
             }
 
         } while (lines[0].equals("quit") == false);
-
+        sc.close();
     }
 }
