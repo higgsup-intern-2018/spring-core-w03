9eb6013491b197d3e58c290d781879b8135e4e71
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-16 22:30:58 +0700

    Edit AppConfig

--- src/main/java/com/higgsup/intern/spring/demo/javaconf/AppConfig.java
+++ src/main/java/com/higgsup/intern/spring/demo/javaconf/AppConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-
     @Bean
     public Dictionary dictionary() {
         return new Dictionary();
