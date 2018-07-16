d61402595d35c8fd1cb59aa5a6f1ec6e45f023b2
Thieu Tu <tu.thieuthanh@gmail.com>
2018-07-16 16:04:47 +0700

    Edit SpringXMLConfigDemo

--- src/main/java/com/higgsup/intern/spring/demo/xmlconf/SpringXMLConfigDemo.java
+++ src/main/java/com/higgsup/intern/spring/demo/xmlconf/SpringXMLConfigDemo.java
@@ -4,13 +4,13 @@ import com.higgsup.intern.spring.demo.pojo.util.InputUtil;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.Scanner;
 
 public class SpringXMLConfigDemo {
     public static void main(String[] args) {
         ApplicationContext appContext =
                 new ClassPathXmlApplicationContext("spring-beans.xml");
         InputUtil inputUtil = appContext.getBean(InputUtil.class);
+        inputUtil.run();
 
     }
 }
