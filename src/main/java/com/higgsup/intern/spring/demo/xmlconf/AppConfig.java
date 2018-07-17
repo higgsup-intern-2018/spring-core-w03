60f8e74db45f244b27d8944d223229da6681d978
NgThuy <ngothuy05111998@gmail.com>
2018-07-17 16:19:42 +0700

    Thuy update spring core - javaconf

---
+++ src/main/java/com/higgsup/intern/spring/demo/xmlconf/AppConfig.java
@@ -0,0 +1,18 @@
+package com.higgsup.intern.spring.demo.xmlconf;
+
+import com.higgsup.intern.spring.demo.pojo.Dictionary;
+import com.higgsup.intern.spring.demo.pojo.ManagerDictionary;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ManagerDictionary managerDictionary(){
+        return new ManagerDictionary();
+    }
+    @Bean
+    public Dictionary dictionary(){
+        return new Dictionary(managerDictionary());
+    }
+}
--- src/main/java/com/higgsup/intern/spring/demo/xmlconf/SpringJavaConfigDemo.java
+++ src/main/java/com/higgsup/intern/spring/demo/xmlconf/SpringJavaConfigDemo.java
@@ -3,11 +3,12 @@ package com.higgsup.intern.spring.demo.xmlconf;
 import com.higgsup.intern.spring.demo.pojo.Dictionary;
 import com.higgsup.intern.spring.demo.pojo.ManagerDictionary;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class SpringXMLConfigDemo {
+public class SpringJavaConfigDemo {
     public static void main(String[] args) {
-        ApplicationContext appContext = new ClassPathXmlApplicationContext("spring-beans.xml");
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
         Dictionary dictionary = appContext.getBean(Dictionary.class);
         dictionary.start();
 
