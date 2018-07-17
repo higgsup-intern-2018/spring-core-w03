1096dad79ae5a9f5a62a37c18da537c6380cf724
DucThanh6111996 <Hoahongden123>
2018-07-17 15:24:02 +0700

    AppConfig

--- src/main/java/com/higgsup/intern/spring/demo/javaconf/AppConfig.java
+++ src/main/java/com/higgsup/intern/spring/demo/javaconf/AppConfig.java
@@ -1,7 +1,19 @@
 package com.higgsup.intern.spring.demo.javaconf;
 
+import com.higgsup.intern.spring.demo.Pojo.DictionaryDAO;
+import com.higgsup.intern.spring.demo.Pojo.Input;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    @Bean
+    public DictionaryDAO dictionaryDAO(){
+        return new DictionaryDAO();
+    }
+
+    @Bean
+    public Input simpleDict(){
+    return new Input();
+    }
 }
