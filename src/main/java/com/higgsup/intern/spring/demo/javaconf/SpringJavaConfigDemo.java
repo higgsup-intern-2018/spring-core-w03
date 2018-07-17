8ee36fc0e6fafc222a0c91a06dd312b97ac2397f
DucThanh6111996 <Hoahongden123>
2018-07-17 15:25:15 +0700

    JavaConfigDemo

--- src/main/java/com/higgsup/intern/spring/demo/javaconf/SpringJavaConfigDemo.java
+++ src/main/java/com/higgsup/intern/spring/demo/javaconf/SpringJavaConfigDemo.java
@@ -1,10 +1,13 @@
 package com.higgsup.intern.spring.demo.javaconf;
 
+import com.higgsup.intern.spring.demo.Pojo.Input;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SpringJavaConfigDemo {
     public static void main(String[] args) {
         ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        Input input = appContext.getBean(Input.class);
+        input.menu();
     }
 }
